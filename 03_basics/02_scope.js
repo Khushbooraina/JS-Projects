2068d5ae16894ff77022a16b3956162a9e4f8005
Khushbooraina <khushbooraina24@gmail.com>
2025-04-01 05:58:47 +0000

    Scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,23 @@
+//var c = 300
+let a  = 300
+
+//scope {}
+
+if (true){
+    let a = 10
+    const b = 20
+    // c = 30  // globally used: it can be accessed even from outside but it should be
+    console.log(" Inner:", a);
+}
+
+
+for (let i = 0; i < array.length; i++){
+    const element = array[i]
+}
+
+
+
+
+console.log(a);
+//console.log(b);
+//console.log(c);
